030034b66fda2ca8f427f02d7028f49568791296
Orgerix <trash@email.com>
2025-09-16 20:40:09 +0200

    Fix message

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://orgerix.github.io/resurgence-helper/'
+  base: 'https://orgerix.github.io/resurgence-helper/',
   plugins: [react()],
 })
